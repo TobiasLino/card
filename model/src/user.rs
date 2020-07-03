3cc3858466447e018b8563489772ef1a56be9337
TobiasLino <tobias.lino@fatec.sp.gov.br>
2020-07-02 23:42:19 -0300

    adição da interface de login

--- model/src/user.rs
+++ model/src/user.rs
@@ -1,18 +1,49 @@
 pub mod address;
+pub mod login;
 
 use address::Address;
+use login::Login;
+
+// TODO: User precisa de um histórico
 
 pub struct User {
     pub name: String,
     pub phone: String,
     pub address: Address,
     pub email: String,
+    // dados de Login
     pub username: String,
-    pub passwd: String
+    passwd: String,
+    // dados de estado
+    is_active: bool,
 }
 
 impl User {
     pub fn data(self) -> String {
         format!("{}\n\t{}", self.username.to_uppercase(), self.passwd.to_lowercase())
+    }
+
+    pub fn status(self) -> bool {
+        self.is_active
+    }
+}
+
+// TODO: Implementar essa interface
+impl Login for User {
+    pub fn get_user_by_username(username: String) -> Option<User> {
+
+    }
+
+    
+    pub fn get_user_by_email(email: String) -> Option<User> {
+
+    }
+
+    pub fn check_passwd(self, passwd: String) -> bool {
+        return self.passwd == passwd
+    }
+
+    pub fn connect(self) -> Result<bool> {
+
     }
 }
